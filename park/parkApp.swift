9c5f8a00b0fec28344d45328525c6b27d70ba901
David Martin <david.martin@gotrendier.com>
2024-02-13 17:58:29 +0100

    Initial Commit

---
+++ park/parkApp.swift
@@ -0,0 +1,29 @@
+//
+// Created by David Martin on 13/2/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct parkApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
